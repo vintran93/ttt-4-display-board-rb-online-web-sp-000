f055dc0a434d92d11025c781d001b6aeb4192009
Vincent Tran <vincentuong@yahoo.com>
2021-04-20 03:10:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@
 
 
 def display_board(board)
-<<<<<<< HEAD
+
 puts " #{board[0]} | #{board[1]} | #{board[2]} " 
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -16,17 +16,9 @@ end
 
 
 
-=======
-" #{board[0]} | #{board[1]}  | #{board[2]} " 
-"-----------"
-" #{board[3]} | #{board[4]}  | #{board[5]} "
-"-----------"
-" #{board[6]} |  #{board[7]} | #{board[8]} "
-
-
-end
 
 
 
 
->>>>>>> 90bd980926d500465615eef0bacbc5f7e3e3deef
+
+
